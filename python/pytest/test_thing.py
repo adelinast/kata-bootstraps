74a8dd96a0b83e712493f2c0aa4a28ebca3f3d41
Adelina <adelinastanciu@gmail.com>
2020-03-31 01:14:54 +0300

    Add passwordvalidator +tests

--- python/pytest/test_thing.py
+++ python/pytest/test_thing.py
@@ -1,11 +1,29 @@
-from thing import Thing
+from thing import PasswordValidator
 
+def test_invalid_empty_password():
+    password_validate = PasswordValidator("")
+    assert False == password_validate.validate()
 
-def test_correct_greeting():
-    thing = Thing("Bob")
-    assert "Hello Bob!" == thing.return_hello_name()
+def test_invalid_less_8_chars():
+    password_validate = PasswordValidator("1234")
+    assert False == password_validate.validate()
 
+def test_valid_more_20chars():
+    password_validate = PasswordValidator("12341234abcccccscdjdsncdj")
+    assert True == password_validate.validate()
 
-def test_fail():
-    thing = Thing("Albert")
-    assert "Wrong!" == thing.return_hello_name()
+def test_invalid_more_8chars_all_upper():
+    password_validate = PasswordValidator("TTTTTTTT")
+    assert False == password_validate.validate()
+
+def test_valid_more_8chars_upper_lower():
+    password_validate = PasswordValidator("TTTTTTTt")
+    assert True == password_validate.validate()
+
+def test_valid_more_8chars_upper_digit():
+    password_validate = PasswordValidator("TTTTTTT1")
+    assert True == password_validate.validate()
+
+def test_valid_more_8chars_lower_digit():
+    password_validate = PasswordValidator("ttttttt1")
+    assert True == password_validate.validate()
--- python/pytest/thing.py
+++ python/pytest/thing.py
@@ -1,7 +1,25 @@
-class Thing:
+class PasswordValidator:
+    def __init__(self, password):
+        self.password = password
 
-    def __init__(self, name):
-        self.name = name
-
-    def return_hello_name(self):
-        return "Hello " + self.name + "!"
+    def isValidPassword(self):
+        hasDigit, hasUpper, hasLower=False, False, False
+        
+        for c in self.password:
+            if c.isupper():
+                hasUpper=True
+            elif c.islower():
+                hasLower=True
+            elif c.isdigit():
+                hasDigit=True
+        
+        return [hasDigit, hasLower, hasUpper].count(True)>=2
+    
+    def validate(self):
+        if len(self.password)>=20:
+            return True
+        if len(self.password)<8:
+            return False
+        
+        return self.isValidPassword()
+    
